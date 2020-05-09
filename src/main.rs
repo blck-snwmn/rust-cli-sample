a57f2f0c9a02f3e0d482c7c448f815b3618b154c
blck-snwmn <whitesnowmancreator@gmail.com>
2020-05-09 23:42:43 +0900

    New cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
     println!("Hello, world!");
+    let default = "test";
+    let first_param = std::env::args().nth(1).unwrap_or(default.to_string());
+    println!("{}", first_param);
 }
