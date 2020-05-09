272a8ce382fdcd19d873455d9b6c83f273c3e81d
blck-snwmn <whitesnowmancreator@gmail.com>
2020-05-10 00:23:07 +0900

    Recive parameter to use StructOpt

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
+use structopt::StructOpt;
+
 fn main() {
     println!("Hello, world!");
-    let default = "test";
-    let first_param = std::env::args().nth(1).unwrap_or(default.to_string());
-    println!("{}", first_param);
+    // let default = "test";
+    // let first_param = std::env::args().nth(1).unwrap_or(default.to_string());
+    // println!("{}", first_param);
+    let param = Parameter::from_args();
+    println!("{:?}", param);
+}
+
+#[derive(Debug, StructOpt)]
+struct Parameter {
+    id: i64,
+    #[structopt(short, long)]
+    name: Option<String>,
+    #[structopt(short = "t", long = "type")]
+    param_type: String,
 }
