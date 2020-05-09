e118e06a0711629c1aaf9f5867be741e6100d59f
blck-snwmn <whitesnowmancreator@gmail.com>
2020-05-10 08:25:31 +0900

    Add sample to read input from pipe

--- src/in-query-out.rs
+++ src/in-query-out.rs
@@ -1,3 +1,29 @@
+use atty::Stream;
+use std::io::{self, BufRead, Write};
 fn main() {
-    println!("hello world!")
+    if atty::is(Stream::Stdin) {
+        // not support for terminal input
+        println!("input is terminal")
+    }
+    println!("input is not terminal");
+    let stdin = io::stdin();
+    let mut reader = io::BufReader::new(stdin.lock());
+
+    let stdout = io::stdout();
+    let mut writter = io::BufWriter::new(stdout.lock());
+
+    let mut buffer = String::new();
+    loop {
+        match reader.read_line(&mut buffer) {
+            Ok(0) => break,
+            Ok(_) => {
+                write!(writter, "{}", buffer).unwrap();
+                buffer.clear();
+            }
+            Err(e) => {
+                println!("{}", e);
+                break;
+            }
+        }
+    }
 }
